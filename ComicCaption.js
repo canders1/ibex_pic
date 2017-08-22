8dbed971f2a1bff200ca7ce023245b0bfe8953b9
Carolyn Anderson <canders277@gmail.com>
2017-08-22 17:16:30 -0400

    test

--- ComicCaption.js
+++ ComicCaption.js
@@ -7,32 +7,15 @@ jqueryWidget: {
     _init: function () {
         var opts = {
             options:     this.options,
-            triggers:    [2],
-            children:    [{html : this.options.html },
-            this.options._dashed ? "DashedSentence" : "FlashSentence",
-                          this.options._dashed ? {
-                                                     s: this.options.s,
-                                                     mode: this.options.mode,
-                                                     display: this.options.display,
-                                                     blankText: this.options.blankText,
-                                                     wordTime: this.options.wordTime,
-                                                     wordPauseTime: this.options.wordPauseTime,
-                                                     sentenceDescType: this.options.sentenceDescType,
-                                                     showAhead: this.options.showAhead,
-                                                     showBehind: this.options.showBehind
-                                                 } :
-                                                 {
-                                                     s: this.options.s,
-                                                     timeout: null, // Already present for 'Question'
-                                                     audio: this.options.audio,
-                                                     audioMessage: this.options.audioMessage,
-                                                     audioTrigger: this.options.audioTrigger
-                                                 },
-                          this.options._dashed ? "!Question" : (this.options.s.audio ? "*Question" : "Question"),
-                          { q:                   this.options.q,
-                            as:                  this.options.as,
-                            hasCorrect:          dget(this.options, "hasCorrect", false),
-                            presentAsScale:      this.options.presentAsScale,
+            triggers:    [1],
+            children:    ["Message", {html : this.options.html,
+            consentRequired: false,
+            transfer: null },
+            "AcceptabilityJudgment", {s: this.options.s,
+            						  q: this.options.q,
+            						  as: this.options.as,
+            						  hasCorrect:          dget(this.options, "hasCorrect", false),
+            						  presentAsScale:      this.options.presentAsScale,
                             presentHorizontally: this.options.presentHorizontally,
                             autoFirstChar:       typeof(this.options.autoFirstChar) == "undefined" ? this.options.presentAsScale : this.options.autoFirstChar,
                             randomOrder:         this.options.randomOrder,
@@ -40,7 +23,8 @@ jqueryWidget: {
                             timeout:             this.options.timeout,
                             instructions:        this.options.instructions,
                             leftComment:         this.options.leftComment,
-                            rightComment:        this.options.rightComment }]/*,
+                            rightComment:        this.options.rightComment
+            						  }]/*,
             manipulators: [
                 [0, function(div) { div.css('font-size', "larger"); return div; }]
             ]*/
@@ -51,18 +35,15 @@ jqueryWidget: {
 },
 
 properties: {
-    obligatory: ["s", "as", "pic"],
+    obligatory: ["s", "as"],
     htmlDescription:
         function (opts) {
-        	//var m = ibex_controller_get_property("Message", "htmlDescription")(opts);
-            var s = ibex_controller_get_property("FlashSentence", "htmlDescription")(opts);
-            var q = ibex_controller_get_property("Question", "htmlDescription")(opts);
+        	var m = ibex_controller_get_property("Message", "htmlDescription")(opts);
+            var a = ibex_controller_get_property("AcceptabilityJudgment", "htmlDescription")(opts);
             var p =
                 $("<p>")
-                //.append($("<p>").append("M: ").append($(m)))
-                .append($("<p>").append("Q: ").append($(q)))
-                .append("<br>").append($("<b>").text("S:"))
-                .append($(s));
+                .append($("<p>").append("M: ").append($(m)))
+                .append($("<p>").append("A: ").append($(a)));
              return p;
         }
 }
