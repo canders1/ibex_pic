bb3bf18d8091a80d45364fc42d79ce1fc7c112aa
Carolyn Anderson <canders277@gmail.com>
2017-08-22 14:26:25 -0400

    testing

--- test_data.js
+++ test_data.js
@@ -1,5 +1,4 @@
-var shuffleSequence = seq("intro", sepWith("sep", seq("practice", rshuffle("s1", "s2"))), sepWith("sep", rshuffle("q1", "q2")));
-var practiceItemTypes = ["practice"];
+var shuffleSequence = seq("intro", sepWith("sep", rshuffle("q1", "q2")));
 
 var defaults = [
     "Separator", {
@@ -33,21 +32,17 @@ var items = [
 
     ["sep", "Separator", { }],
 
-    //
-    // Two "real" (i.e. non-filler) self-paced reading items with corresponding acceptability judgment items.
-    // There are two conditions.
-    //
-
-    [["s1",1], "DashedSentence", {s: "The journalist interviewed an actress who he knew to be shy of publicity after meeting on a previous occasion."},
-               "Question",       {q: "The actress was:", as: ["shy", "publicity-seeking", "impatient"]}],
-    [["s2",1], "DashedSentence", {s: "The journalist interviewed an actress who after meeting on a previous occasion he knew to be shy of publicity."},
-               "Question",       {q: "The actress was:", as: ["shy", "publicity-seeking", "impatient"]}],
+    ["intro", "Form", {
+        html: { include: "intro.html" },
+        validators: {
+            age: function (s) { if (s.match(/^\d+$/)) return true; else return "Bad value for \u2018age\u2019"; }
+        }
+    } ],
 
     // The first question will be chosen if the first sentence from the previous two items is chosen;
     // the second question will be chosen if the second sentence from the previous pair of items is chosen.
 
     [["q1",[100,1]], "AcceptabilityJudgment", {s: "Which actress did the journalist interview after meeting her PA on a previous occasion?"}],
-    [["q1",[100,1]], "ComicCaption", {s: "Which actress did the journalist interview after meeting her PA on a previous occasion?", as: ["shy", "publicity-seeking", "impatient"], q: ["???"]}],
-    [["q2",[100,1]], "AcceptabilityJudgment", {s: "Which actress did the journalist interview her husband after meeting on a previous occasion?"}],
+    [["q2",[100,1]], "ComicCaption", {s: "Which actress did the journalist interview after meeting her PA on a previous occasion?", as: ["shy", "publicity-seeking", "impatient"], q: ["???"], pic: ["huh"]}],
 ];
 
