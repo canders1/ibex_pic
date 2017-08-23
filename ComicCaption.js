daebe29d4782a1a12a1c19f723e16c2258ab8f5e
Carolyn Anderson <canders277@gmail.com>
2017-08-23 11:02:14 -0400

    double message controller works

--- ComicCaption.js
+++ ComicCaption.js
@@ -8,24 +8,29 @@ jqueryWidget: {
     	debugger;
         var opts = {
             options:     this.options,
+            padding: "2em",
             triggers:    [1],
             children:    ["Message", {html : this.options.html,
-            consentRequired: false,
-            transfer: null },
-            "AcceptabilityJudgment", {s: this.options.s,
+                                      transfer: dget(this.options, "transfer", null),
+                                      consentRequired: dget(this.options, "consentRequired",false)
+                                      },
+                                      "Message", {html : this.options.html,
+                                      transfer: dget(this.options, "transfer", "click"),
+                                      consentRequired: dget(this.options, "consentRequired",false)
+                                      },
+            /*"AcceptabilityJudgment", {s: this.options.s,
             						  q: this.options.q,
             						  as: this.options.as,
+                                      instructions:        this.options.instructions,
             						  hasCorrect:          dget(this.options, "hasCorrect", false),
-            						  presentAsScale:      this.options.presentAsScale,
-                            presentHorizontally: this.options.presentHorizontally,
-                            autoFirstChar:       typeof(this.options.autoFirstChar) == "undefined" ? this.options.presentAsScale : this.options.autoFirstChar,
-                            randomOrder:         this.options.randomOrder,
-                            showNumbers:         this.options.showNumbers,
-                            timeout:             this.options.timeout,
-                            instructions:        this.options.instructions,
-                            leftComment:         this.options.leftComment,
-                            rightComment:        this.options.rightComment
-            						  }]
+                                    autoFirstChar:       typeof(this.options.autoFirstChar) == "undefined" ? this.options.presentAsScale : this.options.autoFirstChar,
+                                    showNumbers:         this.options.showNumbers,
+                                    randomOrder:         this.options.randomOrder,
+                                      presentAsScale:      this.options.presentAsScale,
+                                    leftComment:         this.options.leftComment,
+                                    rightComment:        this.options.rightComment,
+                                    timeout:             this.options.timeout
+            						  }*/]
         };
 
         this.element.VBox(opts);
@@ -33,15 +38,16 @@ jqueryWidget: {
 },
 
 properties: {
-    obligatory: ["s", "as"],
+    obligatory: ["s", "as", "q", "html"],
     htmlDescription:
         function (opts) {
         	var m = ibex_controller_get_property("Message", "htmlDescription")(opts);
-            var a = ibex_controller_get_property("AcceptabilityJudgment", "htmlDescription")(opts);
+            var n = ibex_controller_get_property("Message", "htmlDescription")(opts);
+            //var a = ibex_controller_get_property("AcceptabilityJudgment", "htmlDescription")(opts);
             var p =
                 $("<p>")
-                .append($("<p>").append("M: ").append($(m)))
-                .append($("<p>").append("A: ").append($(a)));
+                .append("M: ").append($(m))
+                .append("A: ").append($(n));
              return p;
         }
 }
