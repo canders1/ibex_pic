a0bd45749ef86e9778a23ec5d9e376a451684d10
Carolyn Anderson <canders277@gmail.com>
2017-08-23 11:31:47 -0400

    use all simple controllers

--- ComicCaption.js
+++ ComicCaption.js
@@ -6,34 +6,40 @@ name: "ComicCaption",
 jqueryWidget: {
     _init: function () {
     	debugger;
+        this.cssPrefix = this.options._cssPrefix;
+        this.utils = this.options._utils;
         var opts = {
             options:     this.options,
             padding: "2em",
-            triggers:    [1],
-            children:    ["Message", {html : this.options.html,
-                                      transfer: dget(this.options, "transfer", null),
-                                      consentRequired: dget(this.options, "consentRequired",false)
-                                      },
-                                      "Message", {html : this.options.html,
-                                      transfer: dget(this.options, "transfer", "click"),
-                                      consentRequired: dget(this.options, "consentRequired",false)
-                                      },
-            /*"AcceptabilityJudgment", {s: this.options.s,
-            						  q: this.options.q,
-            						  as: this.options.as,
-                                      instructions:        this.options.instructions,
-            						  hasCorrect:          dget(this.options, "hasCorrect", false),
-                                    autoFirstChar:       typeof(this.options.autoFirstChar) == "undefined" ? this.options.presentAsScale : this.options.autoFirstChar,
-                                    showNumbers:         this.options.showNumbers,
-                                    randomOrder:         this.options.randomOrder,
-                                      presentAsScale:      this.options.presentAsScale,
-                                    leftComment:         this.options.leftComment,
-                                    rightComment:        this.options.rightComment,
-                                    timeout:             this.options.timeout
-            						  }*/]
-        };
-
+            triggers:    [2],
+            children:    [
+                        "Message", 
+                            {html : this.options.html,
+                            transfer: dget(this.options, "transfer", null),
+                            consentRequired: dget(this.options, "consentRequired",false)},
+                        "FlashSentence",
+                            {s: this.options.s,
+                            timeout: null, // Already present for 'Question'
+                            audio: this.options.audio,
+                            audioMessage: this.options.audioMessage,
+                            audioTrigger: this.options.audioTrigger},
+                        "Question", 
+                            {q:                   this.options.q,
+                            as:                  this.options.as,
+                            hasCorrect:          dget(this.options, "hasCorrect", false),
+                            presentAsScale:      this.options.presentAsScale,
+                            presentHorizontally: this.options.presentHorizontally,
+                            autoFirstChar:       typeof(this.options.autoFirstChar) == "undefined" ? this.options.presentAsScale : this.options.autoFirstChar,
+                            randomOrder:         this.options.randomOrder,
+                            showNumbers:         this.options.showNumbers,
+                            timeout:             this.options.timeout,
+                            instructions:        this.options.instructions,
+                            leftComment:         this.options.leftComment,
+                            rightComment:        this.options.rightComment}
+                            ]};
+        console.log("Before");
         this.element.VBox(opts);
+        console.log("After");
     }
 },
 
@@ -42,12 +48,14 @@ properties: {
     htmlDescription:
         function (opts) {
         	var m = ibex_controller_get_property("Message", "htmlDescription")(opts);
-            var n = ibex_controller_get_property("Message", "htmlDescription")(opts);
-            //var a = ibex_controller_get_property("AcceptabilityJudgment", "htmlDescription")(opts);
+            var s = ibex_controller_get_property("FlashSentence", "htmlDescription")(opts);
+            var q = ibex_controller_get_property("Question", "htmlDescription")(opts);
             var p =
-                $("<p>")
-                .append("M: ").append($(m))
-                .append("A: ").append($(n));
+                $(document.createElement("div"))
+                .append($("<p>").append("M: ").append($(m)))
+                .append($("<p>").append("Q: ").append($(q)))
+                .append("<br>").append($("<b>").text("S:"))
+                .append($(s));
              return p;
         }
 }
